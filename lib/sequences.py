34882714fc65a3b3ea4396252ded9b71054a5ba7
gitonga-david <gitonga.muriithi8@gmail.com>
2023-06-24 11:24:30 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    a,b = 0,1
+
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([a])
+    elif length == 2:
+        print([a,b])
+    else:
+        fib = [a,b]
+        for i in range(2,length):
+            fib.append(fib[i-1]+fib[i-2])
+        print(fib)
+
